35ac10ba740b7d3644d72ad1e732e72e5d31025d
Kei <76953169+nakamurakei0978@users.noreply.github.com>
2023-10-17 17:23:24 +0700

    initial

--- route/product.py
+++ route/product.py
@@ -1,58 +1,63 @@
+from datetime import datetime
+from unicodedata import decimal
+
 from flask import Blueprint, render_template, request, redirect, current_app
 from werkzeug.utils import secure_filename
 import os
 from config import execute_query
 
-students = Blueprint('students', __name__)
+products = Blueprint('products', __name__)
 
 
 # ** filter db and get all data and throw to student list page
-@students.route('/admin/student')
-def student():
+@products.route('/admin/product')
+def product():
     # Get the page number from the query parameters (default to 1 if not present)
     page = request.args.get('page', 1, type=int)
 
     # Define the number of records per page
-    records_per_page = 8
+    records_per_page = 7
 
     # Calculate the offset based on the current page
     offset = (page - 1) * records_per_page
 
     # Use the helper function to execute the query
-    rows = execute_query("SELECT * FROM student LIMIT ? OFFSET ?", (records_per_page, offset))
+    rows = execute_query("SELECT *, (select cname from product_category where product.category_id = cid) as cname "
+                         "FROM product LIMIT ? OFFSET ?",
+                         (records_per_page, offset))
 
     # Calculate the total number of pages
-    total_records = execute_query("SELECT COUNT(*) FROM student")[0][0]
+    total_records = execute_query("SELECT COUNT(*) FROM product")[0][0]
     total_pages = (total_records // records_per_page) + (total_records % records_per_page > 0)
 
-    return render_template('admin/student/index.html', rows=rows, page=page, total_pages=total_pages)
+    return render_template('admin/product/index.html', rows=rows, page=page, total_pages=total_pages)
 
 
 # ** load to add student page
-@students.route('/admin/add_student')
-def add_student():
-    return render_template('admin/student/add_student.html')
+@products.route('/admin/add_product')
+def add_product():
+    categories = execute_query("SELECT * FROM product_category")
+    return render_template('admin/product/add_product.html', categories=categories)
 
 
 # ** student added to db
-@students.route('/admin/student_added', methods=['POST'])
-def student_added():
+@products.route('/admin/product_added', methods=['POST'])
+def product_added():
     if request.method == "POST":
         try:
-            firstname = request.form['firstname']
-            lastname = request.form['lastname']
-            birthday = request.form['birthday']
-            gender = request.form['gender']
-            email = request.form['email']
-            phone = request.form['phone']
-            subject = request.form['subject']
+            pname = request.form['productName']
+            category_id = request.form['category']
+            cost = round(float(request.form['cost']), 2) * 100
+            price = round(float(request.form['price']), 2) * 100
+            qty = request.form['qty']
+            desc = request.form['description']
+            created_date = datetime.now().strftime("%d-%m-%y")
 
-            if subject == 'Select subject':
-                subject = ''
+            if category_id == 'Select category':
+                cname = ''
 
             image = request.files.get('image_upload')
 
-            address = request.form['address']
             # check if user input image or not
             if image:
                 # Check the file extension
@@ -65,55 +70,54 @@ def student_added():
                     return 'File size is too large. The maximum file size is 2MB.', 400
 
                 # Save the image to the uploads folder, with fisrtname_lastname.extension as the filename
-                filename = secure_filename(f"{firstname}_{lastname}{extension}")
+                filename = secure_filename(f"{pname}_{cost+price}{extension}")
                 image.save(os.path.join(current_app.config['UPLOAD_FOLDER'], filename))
 
-                query = "INSERT INTO student (firstname,lastname,birthday,gender," \
-                        "email,phone,subject,image,address) VALUES (?,?,?,?,?,?,?,?,?)"
-                params = (firstname, lastname, birthday, gender, email, phone, subject, filename, address)
+                query = "INSERT INTO product (name,category_id,description," \
+                        "qty, cost_in_cent,price_in_cent,image, created_date) VALUES (?,?,?,?,?,?,?,?)"
+                params = (pname, category_id, desc, qty, cost, price, filename, created_date)
             else:
-                query = "INSERT INTO student (firstName,lastName,birthday,gender," \
-                        "email,phone,subject,address) VALUES (?,?,?,?,?,?,?,?)"
-                params = (firstname, lastname, birthday, gender, email, phone, subject, address)
+                query = "INSERT INTO product (name,category_id,description," \
+                        "qty, cost_in_cent,price_in_cent, created_date) VALUES (?,?,?,?,?,?,?)"
+                params = (pname, category_id, desc, qty, cost, price, created_date)
 
             execute_query(query, params, is_insert=True)
 
-            return redirect('/admin/student')
+            return redirect('/admin/product')
         except Exception as e:
             return f"An error occurred: {str(e)}"
 
 
 # ** get student detail from student page or detail page to filter db and throw to edit page
-@students.route('/admin/edit_student', methods=['GET', 'POST'])
-def edit_student():
+@products.route('/admin/edit_product', methods=['GET', 'POST'])
+def edit_product():
     if request.method == 'POST':
-        student_id = request.form.get("id", None)
+        product_id = request.form.get("id", None)
         page = request.form['page_id']
 
         # ** student_sid pass as parameter to prevent sql injection
-        query = f"SELECT * FROM student WHERE id = ?"
-        rows = execute_query(query, (student_id,))
+        query = f"SELECT * FROM product WHERE id = ?"
+        rows = execute_query(query, (product_id,))
+        categories = execute_query("SELECT * FROM product_category")
 
-        return render_template('admin/student/edit_student.html', rows=rows, page=page)
+        return render_template('admin/product/edit_product.html', rows=rows, page=page, categories=categories)
     else:
-        return redirect('/admin/student')
+        return redirect('/admin/product')
 
 
 # ** Student edit or update to db
-@students.route('/admin/student_edited', methods=['POST'])
-def student_edited():
+@products.route('/admin/product_edited', methods=['POST'])
+def product_edited():
     if request.method == "POST":
         try:
-            sid = request.form['id']
+            pid = request.form['id']
             page_id = request.form['page_id']
-            firstname = request.form['firstname']
-            lastname = request.form['lastname']
-            birthday = request.form['birthday']
-            gender = request.form['gender']
-            email = request.form['email']
-            phone = request.form['phone']
-            subject = request.form['subject']
-            address = request.form['address']
+            name = request.form['productName']
+            desc = request.form['description']
+            category_id = request.form['category']
+            cost = round(float(request.form['cost']), 2) * 100
+            price = round(float(request.form['price']), 2) * 100
+            qty = request.form['qty']
 
             image = request.files.get('image_upload')
 
@@ -129,8 +133,8 @@ def student_edited():
                     return 'File size is too large. The maximum file size is 2MB.', 400
 
                 # Get the old image filename from the database
-                old_image_query = "SELECT image FROM student WHERE id = ?"
-                old_image_filename = execute_query(old_image_query, (sid,), is_insert=False)[0]['image']
+                old_image_query = "SELECT image FROM product WHERE id = ?"
+                old_image_filename = execute_query(old_image_query, (pid,), is_insert=False)[0]['image']
 
                 # Delete the old image file
                 if old_image_filename:
@@ -139,63 +143,60 @@ def student_edited():
                         os.remove(old_image_path)
 
                 # Save the image to the uploads folder, with fisrtname_lastname.extension as the filename
-                filename = secure_filename(f"{firstname}_{lastname}{extension}")
+                filename = secure_filename(f"{name}_{cost+price}{extension}")
                 image.save(os.path.join(current_app.config['UPLOAD_FOLDER'], filename))
 
-                query = f"UPDATE student SET " \
-                        f"firstName = ?, " \
-                        f"lastName = ?, " \
-                        f"birthday = ?, " \
-                        f"gender = ?, " \
-                        f"email = ?, " \
-                        f"phone = ?, " \
-                        f"subject = ?, " \
-                        f"image = ?, " \
-                        f"address = ? " \
+                query = f"UPDATE product SET " \
+                        f"name = ?, " \
+                        f"category_id = ?, " \
+                        f"description = ?, " \
+                        f"qty = ?, " \
+                        f"cost_in_cent = ?, " \
+                        f"price_in_cent = ?, " \
+                        f"image = ? " \
                         f"WHERE id = ?"
-                params = (firstname, lastname, birthday, gender, email, phone, subject, filename, address, sid)
+                params = (name, category_id, desc, qty, cost, price, filename, pid)
             else:
-                query = f"UPDATE student SET " \
-                        f"firstName = ?, " \
-                        f"lastName = ?, " \
-                        f"birthday = ?, " \
-                        f"gender = ?, " \
-                        f"email = ?, " \
-                        f"phone = ?, " \
-                        f"subject = ?, " \
-                        f"address = ? " \
+                query = f"UPDATE product SET " \
+                        f"name = ?, " \
+                        f"category_id = ?, " \
+                        f"description = ?, " \
+                        f"qty = ?, " \
+                        f"cost_in_cent = ?, " \
+                        f"price_in_cent = ? " \
                         f"WHERE id = ?"
-                params = (firstname, lastname, birthday, gender, email, phone, subject, address, sid)
+                params = (name, category_id, desc, qty, cost, price, pid)
 
             execute_query(query, params, is_insert=True)
 
-            return redirect('/admin/student?page=' + page_id)
+            return redirect('/admin/product?page=' + page_id)
         except Exception as e:
             return f"An error occurred: {str(e)}"
 
 
 # ** get student details from student page to filter db and throw to detail page
-@students.route('/admin/detail_student', methods=['GET', 'POST'])
-def detail_student():
+@products.route('/admin/detail_product', methods=['GET', 'POST'])
+def detail_product():
     if request.method == 'POST':
-        student_id = request.form.get("id", None)
+        product_id = request.form.get("id", None)
         page = request.form['page_id']
 
         # ** student_sid pass as parameter to prevent sql injection
-        query = f"SELECT * FROM student WHERE id = ?"
-        rows = execute_query(query, (student_id,))
+        query = f"SELECT *, (select cname from product_category where cid==product.category_id) cname " \
+                f"FROM product WHERE id = ?"
+        rows = execute_query(query, (product_id,))
 
-        return render_template('admin/student/detail_student.html', rows=rows, page=page)
+        return render_template('admin/product/detail_product.html', rows=rows, page=page)
     else:
-        return redirect('/admin/student')
+        return redirect('/admin/product')
 
 
 # ** Student delete on db
-@students.route('/admin/delete_student', methods=['POST'])
-def delete_student():
+@products.route('/admin/delete_product', methods=['POST'])
+def delete_product():
     if request.method == "POST":
         try:
-            sid = request.form['id']
+            pid = request.form['id']
             page_id = request.form['page_id']
             image = request.form['image_upload']
 
@@ -205,9 +206,9 @@ def delete_student():
                 if os.path.isfile(image_path):
                     os.remove(image_path)
 
-            query = f"DELETE FROM student WHERE id = ?"
-            execute_query(query, (sid,), is_insert=True)
+            query = f"DELETE FROM product WHERE id = ?"
+            execute_query(query, (pid,), is_insert=True)
 
-            return redirect('/admin/student?page=' + page_id)
+            return redirect('/admin/product?page=' + page_id)
         except Exception as e:
             return f"An error occurred: {str(e)}"
--- route/product_category.py
+++ route/product_category.py
@@ -6,209 +6,122 @@ from werkzeug.utils import secure_filename
 import os
 from config import execute_query
 
-products = Blueprint('products', __name__)
+product_categories = Blueprint('product_categories', __name__)
 
 
 # ** filter db and get all data and throw to student list page
-@products.route('/admin/product')
-def product():
+@product_categories.route('/admin/category')
+def category():
     # Get the page number from the query parameters (default to 1 if not present)
     page = request.args.get('page', 1, type=int)
 
     # Define the number of records per page
-    records_per_page = 8
+    records_per_page = 7
 
     # Calculate the offset based on the current page
     offset = (page - 1) * records_per_page
 
     # Use the helper function to execute the query
-    rows = execute_query("SELECT *, (select cname from product_category where product.category_id = cid) as cname "
-                         "FROM product LIMIT ? OFFSET ?",
+    rows = execute_query("SELECT * FROM product_category LIMIT ? OFFSET ?",
                          (records_per_page, offset))
 
     # Calculate the total number of pages
-    total_records = execute_query("SELECT COUNT(*) FROM product")[0][0]
+    total_records = execute_query("SELECT COUNT(*) FROM product_category")[0][0]
     total_pages = (total_records // records_per_page) + (total_records % records_per_page > 0)
 
-    return render_template('admin/product/index.html', rows=rows, page=page, total_pages=total_pages)
+    return render_template('admin/product_category/index.html', rows=rows, page=page, total_pages=total_pages)
 
 
 # ** load to add student page
-@products.route('/admin/add_product')
-def add_product():
-    categories = execute_query("SELECT * FROM product_category")
-    return render_template('admin/product/add_product.html', categories=categories)
+@product_categories.route('/admin/add_category')
+def add_category():
+    return render_template('admin/product_category/add_category.html')
 
 
 # ** student added to db
-@products.route('/admin/product_added', methods=['POST'])
-def product_added():
+@product_categories.route('/admin/category_added', methods=['POST'])
+def category_added():
     if request.method == "POST":
         try:
-            pname = request.form['productName']
-            category_id = request.form['category']
-            cost = round(float(request.form['cost']), 2) * 100
-            price = round(float(request.form['price']), 2) * 100
-            qty = request.form['qty']
-            desc = request.form['description']
-            created_date = datetime.now().strftime("%d-%m-%y")
+            cname = request.form['cname']
+            desc = request.form['desc']
 
-            if category_id == 'Select category':
-                cname = ''
-
-            image = request.files.get('image_upload')
-
-            # check if user input image or not
-            if image:
-                # Check the file extension
-                _, extension = os.path.splitext(image.filename)
-                if extension.lower() not in ['.jpg', '.png', '.jfif', '.svg', '.jpeg', '.gif']:
-                    return 'Invalid file type. Only jpg, png, and jfif files are allowed.', 400
-
-                # Check the file size (2MB = 2 * 1024 * 1024 bytes)
-                if image.content_length > 2 * 1024 * 1024:
-                    return 'File size is too large. The maximum file size is 2MB.', 400
-
-                # Save the image to the uploads folder, with fisrtname_lastname.extension as the filename
-                filename = secure_filename(f"{pname}_{cost+price}{extension}")
-                image.save(os.path.join(current_app.config['UPLOAD_FOLDER'], filename))
-
-                query = "INSERT INTO product (name,category_id,description," \
-                        "qty, cost_in_cent,price_in_cent,image, created_date) VALUES (?,?,?,?,?,?,?,?)"
-                params = (pname, category_id, desc, qty, cost, price, filename, created_date)
-            else:
-                query = "INSERT INTO product (name,category_id,description," \
-                        "qty, cost_in_cent,price_in_cent, created_date) VALUES (?,?,?,?,?,?,?)"
-                params = (pname, category_id, desc, qty, cost, price, created_date)
+            query = "INSERT INTO product_category (cname,cdescription) VALUES (?,?)"
+            params = (cname, desc)
 
             execute_query(query, params, is_insert=True)
 
-            return redirect('/admin/product')
+            return redirect('/admin/category')
         except Exception as e:
             return f"An error occurred: {str(e)}"
 
 
 # ** get student detail from student page or detail page to filter db and throw to edit page
-@products.route('/admin/edit_product', methods=['GET', 'POST'])
-def edit_product():
+@product_categories.route('/admin/edit_category', methods=['GET', 'POST'])
+def edit_category():
     if request.method == 'POST':
-        product_id = request.form.get("id", None)
+        cid = request.form.get("cid", None)
         page = request.form['page_id']
 
         # ** student_sid pass as parameter to prevent sql injection
-        query = f"SELECT * FROM product WHERE id = ?"
-        rows = execute_query(query, (product_id,))
-        categories = execute_query("SELECT * FROM product_category")
+        query = f"SELECT * FROM product_category WHERE cid = ?"
+        rows = execute_query(query, (cid,))
 
-        return render_template('admin/product/edit_product.html', rows=rows, page=page, categories=categories)
+        return render_template('admin/product_category/edit_category.html', rows=rows, page=page)
     else:
-        return redirect('/admin/product')
+        return redirect('/admin/category')
 
 
 # ** Student edit or update to db
-@products.route('/admin/product_edited', methods=['POST'])
-def product_edited():
+@product_categories.route('/admin/category_edited', methods=['POST'])
+def category_edited():
     if request.method == "POST":
         try:
-            pid = request.form['id']
+            cid = request.form['cid']
             page_id = request.form['page_id']
-            name = request.form['productName']
-            desc = request.form['description']
-            category_id = request.form['category']
-            cost = round(float(request.form['cost']), 2) * 100
-            price = round(float(request.form['price']), 2) * 100
-            qty = request.form['qty']
+            cname = request.form['cname']
+            desc = request.form['desc']
 
-            image = request.files.get('image_upload')
-
-            # check if user input image or not
-            if image:
-                # Check the file extension
-                _, extension = os.path.splitext(image.filename)
-                if extension.lower() not in ['.jpg', '.png', '.jfif', '.svg', '.jpeg', '.gif']:
-                    return 'Invalid file type. Only jpg, png, and jfif files are allowed.', 400
-
-                # Check the file size (2MB = 2 * 1024 * 1024 bytes)
-                if image.content_length > 2 * 1024 * 1024:
-                    return 'File size is too large. The maximum file size is 2MB.', 400
-
-                # Get the old image filename from the database
-                old_image_query = "SELECT image FROM product WHERE id = ?"
-                old_image_filename = execute_query(old_image_query, (pid,), is_insert=False)[0]['image']
-
-                # Delete the old image file
-                if old_image_filename:
-                    old_image_path = os.path.join(current_app.config['UPLOAD_FOLDER'], old_image_filename)
-                    if os.path.isfile(old_image_path):
-                        os.remove(old_image_path)
-
-                # Save the image to the uploads folder, with fisrtname_lastname.extension as the filename
-                filename = secure_filename(f"{name}_{cost+price}{extension}")
-                image.save(os.path.join(current_app.config['UPLOAD_FOLDER'], filename))
-
-                query = f"UPDATE product SET " \
-                        f"name = ?, " \
-                        f"category_id = ?, " \
-                        f"description = ?, " \
-                        f"qty = ?, " \
-                        f"cost_in_cent = ?, " \
-                        f"price_in_cent = ?, " \
-                        f"image = ? " \
-                        f"WHERE id = ?"
-                params = (name, category_id, desc, qty, cost, price, filename, pid)
-            else:
-                query = f"UPDATE product SET " \
-                        f"name = ?, " \
-                        f"category_id = ?, " \
-                        f"description = ?, " \
-                        f"qty = ?, " \
-                        f"cost_in_cent = ?, " \
-                        f"price_in_cent = ? " \
-                        f"WHERE id = ?"
-                params = (name, category_id, desc, qty, cost, price, pid)
+            query = f"UPDATE product_category SET " \
+                    f"cname = ?, " \
+                    f"cdescription = ? " \
+                    f"WHERE cid = ?"
+            params = (cname, desc, (cid,))
 
             execute_query(query, params, is_insert=True)
 
-            return redirect('/admin/product?page=' + page_id)
+            return redirect('/admin/category?page=' + page_id)
         except Exception as e:
             return f"An error occurred: {str(e)}"
 
 
 # ** get student details from student page to filter db and throw to detail page
-@products.route('/admin/detail_product', methods=['GET', 'POST'])
-def detail_product():
+@product_categories.route('/admin/detail_category', methods=['GET', 'POST'])
+def detail_category():
     if request.method == 'POST':
-        product_id = request.form.get("id", None)
+        cid = request.form.get("cid", None)
         page = request.form['page_id']
 
         # ** student_sid pass as parameter to prevent sql injection
-        query = f"SELECT *, (select cname from product_category where cid==product.category_id) cname " \
-                f"FROM product WHERE id = ?"
-        rows = execute_query(query, (product_id,))
+        query = f"SELECT * FROM product_category WHERE cid = ?"
+        rows = execute_query(query, (cid,))
 
-        return render_template('admin/product/detail_product.html', rows=rows, page=page)
+        return render_template('admin/product_category/detail_category.html', rows=rows, page=page)
     else:
-        return redirect('/admin/product')
+        return redirect('/admin/category')
 
 
 # ** Student delete on db
-@products.route('/admin/delete_product', methods=['POST'])
-def delete_product():
+@product_categories.route('/admin/delete_category', methods=['POST'])
+def delete_category():
     if request.method == "POST":
         try:
-            pid = request.form['id']
+            cid = request.form['cid']
             page_id = request.form['page_id']
-            image = request.form['image_upload']
 
-            # Delete the old image file if it's not the default image
-            if image != 'default_img':
-                image_path = os.path.join(current_app.config['UPLOAD_FOLDER'], image)
-                if os.path.isfile(image_path):
-                    os.remove(image_path)
+            query = f"DELETE FROM product_category WHERE cid = ?"
+            execute_query(query, (cid,), is_insert=True)
 
-            query = f"DELETE FROM product WHERE id = ?"
-            execute_query(query, (pid,), is_insert=True)
-
-            return redirect('/admin/product?page=' + page_id)
+            return redirect('/admin/category?page=' + page_id)
         except Exception as e:
             return f"An error occurred: {str(e)}"
